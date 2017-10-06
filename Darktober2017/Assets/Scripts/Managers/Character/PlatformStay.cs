e55c6f3f9cd3e7de87f2f329c56363a2929eed4a
Mikael1987 <miguel.giron5@gmail.com>
2017-10-07 01:22:07 +0200

    Added Hierarchy2 asset, pixelPerfect and tilemaster, also code moving platform and ground check for them.
    Signed-off-by: Mikael1987 <miguel.giron5@gmail.com>

---
+++ Darktober2017/Assets/Scripts/Managers/Character/PlatformStay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStay : MonoBehaviour {
+
+    public GameObject player;
+    void OnCollisionEnter2D(Collision2D platform)
+    {
+
+        if (platform.gameObject.tag == "MovingPlatform")
+        {
+            player.transform.SetParent(platform.transform);
+
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D platform)
+    {
+        if (platform.gameObject.tag == "MovingPlatform")
+        {
+            player.transform.SetParent(null);
+
+        }
+    }
+}
