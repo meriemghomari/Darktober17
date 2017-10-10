3595742757f9bf115bd8a47b56eab93b37bd7ed2
m.ghomari <m.ghomari@student.isartdigital.com>
2017-10-10 23:08:12 +0200

    i need to pull this from somewhere else don't mind lol

---
+++ Darktober2017/Assets/JournalManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalManager : MonoBehaviour {
+    private int num;
+    [SerializeField]
+    public GameObject[,] journalEnteries;
+
+
+    // Use this for initialization
+    void Start () {
+        
+
+
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
