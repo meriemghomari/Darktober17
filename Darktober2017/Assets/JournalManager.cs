1c4bf2e40cfdbf54bb25a29cec8fb08f4fd51357
m.ghomari <m.ghomari@student.isartdigital.com>
2017-10-15 03:10:59 +0200

    journal entries

--- Darktober2017/Assets/JournalManager.cs
+++ Darktober2017/Assets/JournalManager.cs
@@ -2,25 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JournalManager : MonoBehaviour {
-    private int num;
+public class JournalManager : MonoBehaviour
+{
     [SerializeField]
-    public GameObject[,] journalEnteries;
+    GameObject Journal;
+    [SerializeField]
+    GameObject UIJournalButton;
+   public bool showJournal;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      
+        if (collision.gameObject.tag == "Player")
+        {
 
-    // Use this for initialization
-    void Start () {
-        
+            UIJournalButton.SetActive(true);
 
-
-
-
+            showJournal = true;
+        }
+       
 
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+          UIJournalButton.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
+        if ( showJournal && Input.GetKeyDown(KeyCode.X))
+        {
+            //call pause function 
+            Debug.Log("journal should appears");
+            Journal.SetActive(true);
+        }
+        if (Journal.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Journal.SetActive(false);
+        }
+    }
+
+   
 }
